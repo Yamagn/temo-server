9c7cbc35be10625948313cd5026f06f9a3e9110e
Yamagn <syankenpon@gmail.com>
2019-02-06 20:38:15 +0900

    add cms

--- cms/models.py
+++ cms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     roll = models.CharField('役職', max_length=255)
     name = models.CharField("ユーザ名", max_length=255, unique=True)
@@ -10,17 +11,20 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-class Day(models.Model):
-    date = models.DateField('日付', unique_for_date=True)
-    emo = models.IntegerField('エモ度', max_length=6)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, verbose_name="ユーザー", on_delete=models.CASCADE)
+    emo = models.IntegerField("エモ度")
+    day = models.DateField("日付", auto_now_add=True)
 
     def __str__(self):
         return self.date
 
+
 class Team(models.Model):
     created_at = models.DateTimeField("チーム作成日", auto_now_add=True)
     users = models.ForeignKey(User, verbose_name="ユーザー", on_delete=models.CASCADE)
-    calendar = models.ForeignKey(Day, verbose_name="日付", on_delete=models.CASCADE)
+    posts = models.ForeignKey(Post, verbose_name="投稿", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.created_at
